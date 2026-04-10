0b9363b275ca85919b1f933514b4d519e384e9a5
renzonfigueiredo <renzon.figueiredo@estudante.ifms.edu.br>
2026-04-10 11:22:56 -0400

    today

--- backend/src/todos/use-cases/delete-todo.use-case.ts
+++ backend/src/todos/use-cases/delete-todo.use-case.ts
@@ -1,26 +1,30 @@
 import { Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { DeleteTodoRepository } from "../repository";
+import { FindTodoByIdRepository } from "../repository/find-todo-by-id.repository";
 
 @Injectable()
-export class DeleteTodoUseCases {
+export class DeleteTodoUseCase {
     constructor(
-        private readonly deleteTodoRepository: DeleteTodoRepository,
-        private readonly logger: Logger,
+        private readonly DeleteTodoRepository: DeleteTodoRepository,
+        private readonly FindTodoByIdRepository: FindTodoByIdRepository,
+        private readonly logger: Logger
     ) {}
+
     async execute(id: string) {
         try {
             this.logger.log('Deleting toDo...');
-            const todo = await this.deleteTodoRepository.findById(id);
+            const todo = await this.FindTodoByIdRepository.findById(id);
 
             if (!todo) {
-                throw new NotFoundException("ToDo not found");
+                throw new NotFoundException("toDo not found");
             }
-            await this.deleteTodoRepository.delete(id);
-            this.logger.log('ToDo deleted successfully.');
+
+            await this.DeleteTodoRepository.delete(id);
+            this.logger.log('toDo deleted successfully');
             return todo;
         } catch (error) {
             this.logger.error(error);
-            throw error;
+            throw new Error("Failed to delete toDo");
         }
     }
 }
--- backend/src/todos/use-cases/update-todo.use-case.ts
+++ backend/src/todos/use-cases/update-todo.use-case.ts
@@ -1,28 +1,31 @@
 import { Injectable, Logger, NotFoundException } from "@nestjs/common";
-import { UpdateTodoRepository } from "../repository";
 import { UpdateTodoDto } from "../dto/update-todo.dto";
+import { UpdateTodoRepository } from "../repository";
+import { FindTodoByIdRepository } from "../repository/find-todo-by-id.repository";
 
 @Injectable()
-export class UpdateTodoUseCases {
+export class UpdateTodoUseCase {
     constructor(
         private readonly updateTodoRepository: UpdateTodoRepository,
-        private readonly logger: Logger,
+        private readonly FindTodoByIdRepository: FindTodoByIdRepository,
+        private readonly logger: Logger
     ) {}
+
     async execute(id: string, data: UpdateTodoDto) {
         try {
-            this.logger.log('Updating toDo...');
-            const todo = await this.updateTodoRepository.execute(id, data);
+            const todo = await this.FindTodoByIdRepository.findById(id);
+
             if (!todo) {
-                throw new NotFoundException("ToDo not found");
-                        }
-                        
-                        
-            this.logger.log('ToDo updated successfully');
+                throw new NotFoundException("toDo not found");
+            }
+
+            this.logger.log('Updating toDo...');
+            await this.updateTodoRepository.execute(id, data);
+            this.logger.log('toDo updated successfully');
             return todo;
         } catch (error) {
             this.logger.error(error);
-            throw error;
+            throw new Error("Failed to update toDo");
         }
     }
 }
-
