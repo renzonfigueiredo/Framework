c3023cde4631105b6b67fc4df651a160c2460ed9
renzonfigueiredo <renzon.figueiredo@estudante.ifms.edu.br>
2026-05-06 12:21:03 -0400

    auth

---
+++ backend/src/auth/repository/create-user.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/databases/prisma.database";
+
+
+type CreateUserParams = {
+    name: string;
+    email: string;
+    passwordHash: string;
+};
+
+@Injectable()
+export class CreateUserRepository { 
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(data: CreateUserParams) {
+        return await this.prisma.user.create({
+            data,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+        });
+    }
+}
