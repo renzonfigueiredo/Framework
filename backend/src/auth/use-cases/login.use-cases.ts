be2f1903f8a239c3458e47e0d3f1b4994632866e
renzonfigueiredo <renzon.figueiredo@estudante.ifms.edu.br>
2026-05-15 12:18:55 -0400

    loginusecases

--- backend/src/auth/use-cases/login.use-cases.ts
+++ backend/src/auth/use-cases/login.use-cases.ts
@@ -1,1 +1,35 @@
-//importa
+import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
+import { FindUserByEmailRepository } from "../repository";
+import { JwtService } from "@nestjs/jwt";
+import { LoginDto } from "../dto/login.dto";
+import { compare, hash } from "bcrypt";
+@Injectable()
+export class LoginUseCase {
+    
+    constructor(
+        private readonly findUserByEmailRepository: FindUserByEmailRepository,
+        private readonly logger: Logger,
+        private readonly jwtService: JwtService
+    ){}
+
+    async execute(data: LoginDto) {
+        this.logger.log('Attempting to log in user: ' + data.email);
+
+        const user = await this.findUserByEmailRepository.findByEmail(data.email);
+        if (!user) {
+            throw new Error('Invalid credentials: Email or Password');
+        }
+        const isPasswordValid = await compare(data.password, user.passwordHash);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials: Email or Password');
+        }
+
+        const payload = { sub: user.id, email: user.email };
+         const acessToken = this.jwtService.sign(payload);
+
+         this.logger.log('User logged in successfully: ' + data.email);
+        return { acessToken,
+                user: { id: user.id, name: user.name, email: user.email }
+        };
+    }
+}   
