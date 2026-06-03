e7b144008c7999fa4f090d828c672fcdf7eebdc7
renzonfigueiredo <renzon.figueiredo@estudante.ifms.edu.br>
2026-06-03 12:24:22 -0400

    feat: integrate AuthModule and enhance DTO validation for login and registration

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class LoginDto {
-  email: string;
-  password: string;
+ @IsString()
+   @IsNotEmpty()
+   email: string;
+   @IsString()
+   @IsNotEmpty()
+   password: string;
 }
--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsString()
+  @IsNotEmpty()
   email: string;
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
